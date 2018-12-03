ddc03d569837acd45f737fdb6df4a6f9afe3a855
Axel Jacquot <axel.jacquot@ynov.com>
2018-12-03 19:55:09 +0100

    Modifief Code

--- Configuration_STM32/configuration.h
+++ Configuration_STM32/configuration.h
@@ -1,7 +1,7 @@
 #ifndef CONFIGURATION_H_
 #define CONFIGURATION_H_
 
-
+#include "stm32f4xx_hal.h"
 
 #define LED_Port	GPIOD
 #define Green			GPIO_PIN_12
@@ -12,6 +12,14 @@
 #define TIMER4		4
 #define Led_all		Green | Orange | Red | Blue
 
+/*void Timer_Mode_OCPWM_Config(TIM_HandleTypeDef * Timer, unsigned int mode, unsigned int polarity);
+void Timer_PWM_OCPWM_Pulse_Channel(TIM_HandleTypeDef * Timer, unsigned int channel, unsigned int pulse);
+void ENABLE_CLK_TIMER_CHOICE(TIM_TypeDef * timer);
+void Timer_Config(TIM_HandleTypeDef * Timer, TIM_TypeDef * timer_choice, unsigned int prescaler, unsigned int period);
+void Timer_Mode_IC_Config(TIM_HandleTypeDef * Timer, unsigned int polarity, unsigned int filter, unsigned int selection, unsigned int prescaler);
+
+void ENABLE_CLK_GPIO(GPIO_TypeDef * GPIO);
+void GPIO_Configuration(GPIO_TypeDef * GPIO, unsigned int mode, unsigned int Pin, unsigned int alternate);*/
 
 
 
